b0dad023316617f1e4a013ac34665c0dbc4ea327
jhughes <jhughes@student.42adel.org.au>
2024-05-25 11:52:36 +0930

    Rewrote export to handle multiple exports in one command, and to check if the key is valid (contains letters, numbers, or _, except not starting with number).

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,31 +6,89 @@
 /*   By: jhughes <jhughes@student.42adel.org.au>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 12:45:07 by jhughes           #+#    #+#             */
-/*   Updated: 2024/05/22 21:49:59 by jhughes          ###   ########.fr       */
+/*   Updated: 2024/05/25 11:13:37 by jhughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+/// @brief Prints an error to the Standard Error.
+/// @param env The minishell environment, to get the shell name.
+/// @param path The command or path used to cause an error.
+/// @param error The error message to display.
+/// @return 
+static int	error(t_environment *env, const char *path, const char *error)
+{
+	char	*message;
+
+	message = ft_strconcat((char *) env->shell, ": export: \'",
+			path, "\': ", error, NULL);
+	if (!message)
+	{
+		perror("minishell: cd");
+		exit(EXIT_FAILURE);
+	}
+	ft_putendl_fd(message, STDERR_FILENO);
+	free(message);
+	return (EXIT_FAILURE);
+}
+
+/// @brief Determines if a key is valid: only contains letters, numbers, and 
+/// underscore. Cannot start with a number.
+/// @param str The key to check.
+/// @return 1 if valid, 0 otherwise.
+int	valid_identifier(char *str)
+{
+	int	end;
+	int	index;
+
+	if (!str || ft_isdigit(*str))
+		return (FALSE);
+	end = ft_strfind(str, "=");
+	if (end == -1)
+		end = ft_strlen(str);
+	index = 0;
+	if (ft_isdigit(str[index]))
+		return (FALSE);
+	while (index < end)
+	{
+		if (!ft_isalnum(str[index]) && str[index] != '_')
+			return (FALSE);
+		index++;
+	}
+	return (TRUE);
+}
 
 /// @brief Builtin: Adds/sets the value of a key/value pair in the form of
 /// ``key=value`` to the minishell environment
 /// @param env The minishell environment.
 /// @param key_value The key/value pair to add/set in the environment.
 /// @return Exit code:
-/// 0 on success. 1 if set_var failed.  2 if invalid key. 3 if malloc failed.
+/// 0 on success. 1 if set_var failed.  2 if malloc failed.
 int	builtin_export(t_command command, t_environment *env)
 {
-	char	*key;
+	int		exit_code;
+	char	**cmd;
 	int		index;
-	int		status;
+	char	*key;
 
-	index = ft_strfind(command.command[1], "=");
-	if (index == -1)
-		return (1);
-	key = ft_substr(command.command[1], 0, index);
-	if (!key)
-		return (2);
-	status = set_var(env, key, ft_strchr(command.command[1], '=') + 1);
-	free(key);
-	return (status);
+	exit_code = EXIT_SUCCESS;
+	cmd = command.command;
+	cmd++;
+	while (*cmd)
+	{
+		index = ft_strfind(*cmd, "=");
+		if (!valid_identifier(*cmd))
+			exit_code = error(env, *cmd, "not a valid identifier");
+		else if (index != -1)
+		{
+			key = ft_substr(*cmd, 0, index);
+			if (!key)
+				return (EXIT_NO_MEMORY);
+			set_var(env, key, ft_strchr(*cmd, '=') + 1);
+			free(key);
+		}
+		cmd++;
+	}
+	return (exit_code);
 }
